b05349363b9e9733d9aaafea8f722d529eab28b4
Pipe7-sudo <pipeloluwaakinmusire@gmail.com>
2024-11-24 22:15:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const contactButton = document.getElementById('con')
+const contactDiv = document.querySelector('.cont')
+contactButton.addEventListener('click', ()=>
+{
+    contactDiv.scrollIntoView({behavior: 'smooth'})
+})
+
+const contactAutton = document.getElementById('w2')
+const contactSiv = document.querySelector('.abt')
+contactAutton.addEventListener('click', ()=>
+{
+    contactSiv.scrollIntoView({behavior: 'smooth'})
+})
+
+const contactCutton = document.getElementById('w3')
+const contactFiv = document.querySelector('.ex')
+contactCutton.addEventListener('click', ()=>
+{
+    contactFiv.scrollIntoView({behavior: 'smooth'})
+})
+
+const contactDutton = document.getElementById('w4')
+const contactJiv = document.querySelector('.skils')
+contactDutton.addEventListener('click', ()=>
+{
+    contactJiv.scrollIntoView({behavior: 'smooth'})
+})
+
+const contactEutton = document.getElementById('w5')
+const contactKiv = document.querySelector('.ex2')
+contactEutton.addEventListener('click', ()=>
+{
+    contactKiv.scrollIntoView({behavior: 'smooth'})
+})
+
+const contactIutton = document.getElementById('w6')
+const contactIiv = document.querySelector('.ox')
+contactIutton.addEventListener('click', ()=>
+{
+    contactIiv.scrollIntoView({behavior: 'smooth'})
+})
